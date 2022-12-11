26dcfa1021c4926a82bedb47f0d835a5bb90c544
Avialmonanay <108016215+Avialmonanay@users.noreply.github.com>
2022-12-11 14:09:36 -0700

    portfolioContainer.js built

---
+++ components/PortfolioContainer.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import NavTabs from './NavTabs';
+import About from './pages/AboutMe';
+import Portfolio from './pages/Portfolio';
+import Contact from './pages/Contact';
+import Resume from './pages/Resume';
+import Footer from './Footer'
+
+export default function PortfolioContainer() {
+  const [currentPage, setCurrentPage] = useState('Home');
+
+  // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
+  const renderPage = () => {
+    if (currentPage === 'About') {
+      return <About />;
+    }
+    if (currentPage === 'Portfolio') {
+      return <Portfolio />;
+    }
+    if (currentPage === 'Contact') {
+      return <Contact />;
+    }
+    return <Resume />;
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
+  return (
+    <div>
+      <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
+      {renderPage()}
+      <Footer/>
+    </div>
+  );
+}
